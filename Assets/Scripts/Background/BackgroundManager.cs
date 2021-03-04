a5be302665dc55711ea51de33c8df1646f30e60f
Jukol <yukhymster@gmail.com>
2021-03-04 21:37:26 +0200

    Background size adjustable to screen

--- Assets/Scripts/Background/BackgroundManager.cs
+++ Assets/Scripts/Background/BackgroundManager.cs
@@ -4,15 +4,78 @@ using UnityEngine;
 
 public class BackgroundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static BackgroundManager _instance;
+    public static BackgroundManager Instance => _instance;
+
+    private void Awake()
     {
-        
+        _instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private int _backgroundsNumber;
+    [SerializeField] private GameObject _backgroundPrefab;
+    
+    private GameObject[] _backgrounds;
+
+    private SpriteRenderer _spriteRenderer;
+    private Sprite _sprite;
+
+    private void Start()
     {
-        
+        _backgrounds = new GameObject[_backgroundsNumber];
+
+        for (int i = 0; i < _backgroundsNumber; i++)
+        {
+            _backgrounds[i] = Instantiate(_backgroundPrefab, this.transform);
+        }
+
+        _spriteRenderer = _backgrounds[0].GetComponent<SpriteRenderer>();
+        _sprite = _spriteRenderer.sprite;
+
+        GetBackgroundsToStartPosition();
+    }
+
+    private Vector2 BackgroundSize()
+    {
+        float worldScreenHeight = Camera.main.orthographicSize * 2;
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        float resizeFactor = worldScreenWidth / _sprite.bounds.size.x;
+
+        for (int i = 0; i < _backgrounds.Length; i++)
+        {
+            _backgrounds[i].transform.localScale = new Vector3(resizeFactor, resizeFactor, 1);
+        }
+
+        return new Vector2(_spriteRenderer.bounds.size.x, _spriteRenderer.bounds.size.y);
+    }
+
+    private void GetBackgroundsToStartPosition()
+    {
+        for (int i = 0; i < _backgrounds.Length; i++)
+        {
+            if (i == 0)
+            {
+                _backgrounds[i].transform.position = new Vector2(0, 0);
+            }
+            else
+            {
+                _backgrounds[i].transform.position = new Vector2(0, _backgrounds[i - 1].transform.position.y + BackgroundSize().y);
+            }
+        }
+    }
+
+    public float MoveUpY()
+    {
+        float highestY = -100f;
+
+        for (int i = 0; i < _backgroundsNumber; i++)
+        {
+            if (_backgrounds[i].transform.position.y > highestY)
+            {
+                highestY = _backgrounds[i].transform.position.y;
+            }
+        }
+        return highestY + BackgroundSize().y;
     }
 }
---
+++ Assets/Scripts/Background/BackgroundMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMovement : MonoBehaviour
+{
+    protected float _backgroundSpeed = 0.5f;
+    private SpriteRenderer _spriteRenderer;
+    private float _myHeight;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _myHeight = _spriteRenderer.bounds.size.y;
+    }
+    protected void LateUpdate()
+    {
+        transform.position += -transform.up * Time.deltaTime * _backgroundSpeed;
+
+        if (transform.position.y <= -_myHeight)
+        {
+            var moveUpY = BackgroundManager.Instance.MoveUpY();
+            transform.position = new Vector2(0, moveUpY-0.01f);
+        }
+    }
+}
